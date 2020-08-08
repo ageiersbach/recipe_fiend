7dec7fa1cc5b419170c82e5f834b6da4a4df5208
Anne Geiersbach <annasazi@gmail.com>
2020-08-08 12:25:44 -0400

    Add react component for flash messages

--- app/javascript/components/FlashNotice.js
+++ app/javascript/components/FlashNotice.js
@@ -1,13 +1,29 @@
 import React from "react"
 import PropTypes from "prop-types"
 class FlashNotice extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { message: props.message };
+  }
+
+  clearMessage() {
+    this.setState({
+      message: null
+    });
+  }
+
   render () {
+    if (!this.state.message) {
+      return null;
+    }
+
     return (
       <div class="flash-message">
         <p class="notice">
-          {this.props.message}
+          {this.state.message}
         </p>
-        <button class="btn btn-sm">
+        <button class="btn btn-sm" onClick={() => this.clearMessage()}>
           <i class="fas fa-times"></i>
         </button>
       </div>
